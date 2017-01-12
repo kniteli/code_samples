cdfb0f650c547021bd9d3b75d46a9201709ec590
Roderick Gibson <kniteli@gmail.com>
2017-01-11 22:18:36 -0800

    wordpress login plugin
    expose api properly
    added login functionality
    formatting/comments

--- php/wordpress/wp_auth_api.php
+++ php/wordpress/wp_auth_api.php
@@ -20,40 +20,43 @@ register_activation_hook( __FILE__, array(WPAPIPlugin::get_instance(), 'install'
 register_deactivation_hook( __FILE__, array(WPAPIPlugin::get_instance(), 'uninstall'));
 add_action('plugins_loaded', array ( WPAPIPlugin::get_instance(), 'run' ));
 
+//placeholder secret, in actual practice this would be generated per user we wanted
+//to give access to the api
+const API_SECRET = 'tombom';
+
 class WPAPIPlugin {
 
-	protected static $instance = NULL;
-
-    //make sure plugin is singleton in order to handle activation and deactivation
-	public static function get_instance() {
-		NULL === self::$instance && self::$instance = new self;
-		return self::$instance;
-	}
-
-    protected $api_prefix = '/wp-auth-api';
-
-    protected $paths = array(
-        'login' => '/login'
-    );
+    protected $api_prefix = 'wp-auth-api';
 
     public function install() {
-
+        $this->init();
+        flush_rewrite_rules(true);
     }
 
     public function uninstall() {
-
+        flush_rewrite_rules(true);
     }
 
     public function run() {
         //watch for endpoint requests
         add_action('parse_request', array($this, 'watch_requests'), 0);
+        add_action('init', array($this, 'init'));
+
+        add_filter('query_vars', function($qv) {
+            $qv[] = 'api_endpoint';
+            return $qv;
+        });
+    }
+
+    public function init() {
+        add_rewrite_rule("^{$this->api_prefix}/([a-zA-Z]+)/?", 'index.php?api_endpoint=$matches[1]', 'top');
     }
 
     /**
     * Listen for requests to our endpoint
     */
-    public function watch_requests() {
-        $endpoint = $this->parse_request();
+    public function watch_requests($wp) {
+        $endpoint = $wp->query_vars['api_endpoint'];
         if($endpoint) {
             //parse our incoming request data
             $req = json_decode(file_get_contents('php://input'));
@@ -64,7 +67,7 @@ class WPAPIPlugin {
             //make sure the request is coming from a trusted source
             if($this->validate_api_secret($req->secret)) {
                 switch($endpoint) {
-                    case $this->paths['login']:
+                    case 'login':
                         $response = $this->login($req->payload);
                     break;
                     default:
@@ -80,6 +83,7 @@ class WPAPIPlugin {
                 $this->send_response(401, $response);
             }
         }
+        return $query;
     }
 
     /**
@@ -98,7 +102,7 @@ class WPAPIPlugin {
         );
 
         $bad_auth_message = "Username or password is incorrect.";
-        $user = get_user_by( 'email', $data->username );
+        $user = get_user_by( 'login', $data->username );
         if ( $user ) {
             if(wp_check_password( $data->password, $user->data->user_pass, $user->ID)) {
                 $response['user_id'] = $user->ID;
@@ -113,6 +117,14 @@ class WPAPIPlugin {
         return $response;
     }
 
+    protected static $instance = NULL;
+
+    //make sure plugin is singleton in order to handle activation and deactivation properly
+    public static function get_instance() {
+        NULL === self::$instance && self::$instance = new self;
+        return self::$instance;
+    }
+
     /**
     * Send JSON response to client
     * @param  $code => http response code
@@ -125,16 +137,21 @@ class WPAPIPlugin {
         exit;
     }
 
-    private validate_api_secret($secret) {
-        return $secret === API_SECRET;
+    private function get_auth_cookie($user_id, $expires = false) {
+        if(!$expires) {
+            //30 days, possibly want a parameter to make this session only
+            $expires = time()+30*24*60*60;
+        }
+
+        //wordpress uses LOGGED_IN_COOKIE as the auth cookie name,
+        //it is defined in wp-includes/default-constants.php
+        return array(
+            "name" => LOGGED_IN_COOKIE,
+            "value" => wp_generate_auth_cookie($user_id, $expires, 'logged_in'),
+        );
     }
 
-    private function parse_request() {
-        $endpoint = false;
-        $path = strtok($_SERVER["REQUEST_URI"],'?');
-        if ( strrpos($path, $this->api_prefix, -strlen($path)) !== FALSE ) { // path starts with api_prefix
-            $endpoint = substr($path, strlen($this->api_prefix));
-        }
-        return $endpoint;
+    private function validate_api_secret($secret) {
+        return $secret === API_SECRET;
     }
 }
